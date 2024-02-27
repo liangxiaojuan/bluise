a740d110d942fd0662fda21deccc5a2184a7d8cf
杨怡 <639293382@qq.com>
2024-02-27 21:45:56 +0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
   <section class="home">
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
-        <div  class="home__welcome " >
+        <div  class="home__welcome text-4" >
           Welcome to KZPporkChef, this is not only a cooking application, but also your best assistant for grilling pork chops. In the unique world of taste, we take you to explore the mystery of delicious food.
 
 KZPporkChef is unusual because it is not just a simple list of recipes, but a true master of pork. Here, you are the director of cooking, and our app is your right-hand assistant, allowing you to master every pork chop with ease.
@@ -15,7 +15,9 @@ KZPporkChef is your secret weapon to challenge the ultimate in cooking and your 
         </div>
 
         <div class="mb-12 xl:mb-0">
-          <h2 >email: huynhtrannhu864@gmail.com</h2>
+          <br>
+          <br>
+          <h2 class="text-6">Email: huynhtrannhu864@gmail.com</h2>
 
           
         </div>
